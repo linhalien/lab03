5a86344c96948d78c2201821dbdca31a83ae2bec
linhalien <nguyenngoclinh13718@gmail.com>
2024-11-22 18:44:54 +0700

    Add files via upload

---
+++ Lab03/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -0,0 +1,33 @@
+package hust.soict.dsai.test.disc;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class TestPassingParameter {
+
+    public static void main(String[] args) {
+        // Create two DigitalVideoDisc objects
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        // Call the swap method
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        // Call the changeTitle method
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
